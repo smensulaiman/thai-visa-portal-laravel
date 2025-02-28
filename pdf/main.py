4d024c290fd30e5c7164ef6fa0ad0fc488c520aa
MD SULAIMAN <smensulaiman007@gmail.com>
2025-02-28 11:56:42 +0900

    python script brush up

--- pdf/main.py
+++ pdf/main.py
@@ -4,8 +4,10 @@ import sys
 from tika import parser
 
 argList = sys.argv
-arg1 = argList[1]
+pdfFile = argList[1]
 
-parsed = parser.from_file('C:/wamp64/www/thai-mvc/assets/uploads/'+arg1)
-print(parsed["metadata"])
+# print(pdfFile)
+
+parsed = parser.from_file(pdfFile)
+# print(parsed["metadata"])
 print(parsed["content"])
